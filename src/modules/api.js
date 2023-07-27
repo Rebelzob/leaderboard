a420fce55cb2b461909b894f557c3dbce86ca57a
Rebelzob <karimdavid07@gmail.com>
2023-07-27 21:53:35 +0100

    Added API GET and POST requests

---
+++ src/modules/api.js
@@ -0,0 +1,32 @@
+const postRequest = async (data) => {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5AVHts8XtlYBfGgCSSC7/scores/', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    return response.json();
+  } catch (error) {
+    return error;
+  }
+};
+
+const getRequest = async () => {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5AVHts8XtlYBfGgCSSC7/scores/');
+    const data = await response.json();
+    const scores = document.querySelector('.table-container');
+    
+    data.result.forEach((element) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `<td>${element.user}</td><td>${element.score}</td>`;
+      scores.appendChild(row);
+    });
+  }catch (error) {
+    return error;
+  }
+}
+
+export { postRequest, getRequest };
