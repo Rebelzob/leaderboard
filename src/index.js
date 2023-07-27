0076157d555f7f3e63c49a2f36f80cb2f463324a
Rebelzob <karimdavid07@gmail.com>
2023-07-27 21:54:12 +0100

    Added Refresh and Submit functionalities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './index.css';
+import { getRequest, postRequest } from './modules/api';
+
+const form = document.querySelector('.form');
+const scores = document.querySelector('.table-container');
+const refresh = document.querySelector('#refresh-btn');
+const submit = document.getElementById('submit-btn');
+
+window.addEventListener = ('DOMContentLoaded', () => {
+  getRequest();
+});
+
+refresh.addEventListener('click', () => {
+  scores.innerHTML = '';
+  getRequest();
+});
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#name').value;
+  const score = document.querySelector('#score').value;
+  const data = {
+    user: name,
+    score: score,
+  };
+  postRequest(data);
+  form.reset();
+});
